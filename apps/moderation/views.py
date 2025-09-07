f4c9ba42b5e57b9fcd32efa735c436d6b3a28b15
Mirat Can Bayrak <miratcanbayrak@gmail.com>
2025-09-07 17:15:54 +0300

    Add report form and form-based create view

--- apps/moderation/views.py
+++ apps/moderation/views.py
@@ -1,25 +1,21 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView
-from django.shortcuts import redirect
-from django.contrib import messages
+from django.shortcuts import render
+from django.views.generic import FormView
+
 from apps.submissions.models import Report
+from .forms import ReportForm
 
 
-class ReportCreateView(LoginRequiredMixin, TemplateView):
-    template_name = 'moderation/report_success.html'
+class ReportCreateView(LoginRequiredMixin, FormView):
+    template_name = 'moderation/report_form.html'
+    form_class = ReportForm
 
-    def post(self, request, *args, **kwargs):
-        target_type = request.POST.get('target_type')
-        target_id = request.POST.get('target_id')
-        reason = (request.POST.get('reason') or '').strip()
-        if not (target_type and target_id and reason):
-            messages.error(request, 'Eksik bilgi: target ve reason gerekli.')
-            return redirect('home')
-        try:
-            tid = int(target_id)
-        except ValueError:
-            messages.error(request, 'Geçersiz hedef.')
-            return redirect('home')
-        Report.objects.create(target_type=target_type, target_id=tid, reporter=request.user, reason=reason)
-        return self.get(request, *args, **kwargs)
+    def form_valid(self, form):
+        Report.objects.create(
+            target_type=form.cleaned_data['target_type'],
+            target_id=form.cleaned_data['target_id'],
+            reporter=self.request.user,
+            reason=form.cleaned_data['reason'],
+        )
+        return render(self.request, 'moderation/report_success.html')
 
